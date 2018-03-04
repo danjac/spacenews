74d76d0498f54cff3287e18c7b6369b3311d616b
Dan Jacob <danjac2018@gmail.com>
2018-03-04 17:21:56 +0200

    Tidyup

--- frontend/pages/login.vue
+++ frontend/pages/login.vue
@@ -14,7 +14,7 @@
 
 <script>
 export default {
-  middleware: ['auth'],
+  // middleware: ['auth'],
   data () {
     return {
       form: {
--- frontend/pages/signup.vue
+++ frontend/pages/signup.vue
@@ -10,8 +10,16 @@
       <b-form-group label="Password" :invalid-feedback="errors.first('password')" :state="!errors.has('password')">
         <b-form-input type="password" data-vv-value-path="innerValue" name="password" v-model="form.password" v-validate="'required'"></b-form-input>
       </b-form-group>
-      <b-form-group label="Repeat Password" :invalid-feedback="errors.first('password_rpt')" :state="!errors.has('password_rpt')">
-        <b-form-input type="password" data-vv-value-path="innerValue" name="password_rpt" v-validate="'confirmed:target=password'"></b-form-input>
+      <b-form-group
+          label="Repeat Password"
+          :invalid-feedback="errors.first('password_rpt')"
+          :state="!errors.has('password_rpt')">
+        <b-form-input
+          type="password"
+          data-vv-as="password"
+          data-vv-value-path="innerValue"
+          name="password_rpt"
+          v-validate="'confirmed:target=password'"></b-form-input>
       </b-form-group>
       <b-button variant="primary" size="lg" type="submit">Signup</b-button>
     </b-form>
@@ -20,13 +28,13 @@
 
 <script>
 export default {
-  // middleware: ['auth'],
   data () {
     return {
       form: {
         username: '',
         email: '',
-        password: ''
+        password: '',
+        password_rpt: ''
       }
     }
   },
@@ -39,7 +47,7 @@ export default {
       try {
         await this.$axios.post('/api/auth/users/create/', this.form)
         this.$toast.success('Congrats! Please log in with your new credentials.')
-        this.$router.push('/login/')
+        this.$router.push('/login/?redirect=/')
       } catch(e) {
         // tbd: handle form errors
         if (e.response.data && e.response.data) {
