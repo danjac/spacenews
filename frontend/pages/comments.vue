3f66948cf8170ab367a2a5acaca2495a185ded40
Dan Jacob <danjac2018@gmail.com>
2018-03-04 17:27:24 +0200

    Comments page

--- frontend/pages/comments.vue
+++ frontend/pages/comments.vue
@@ -1,5 +1,36 @@
 <template>
   <div>
-    comments go here...
+  <b-card class="mt-4" v-for="comment in comments" :key="comment.id">
+      <p><nuxt-link :to="`/posts/${comment.post}/`">{{ comment.content }}</nuxt-link></p>
+    <p>
+      Posted by [ <nuxt-link to="/">{{ comment.author.username }}</nuxt-link> ]
+    </p>
+   </b-card>
   </div>
 </template>
+
+<script>
+
+export default {
+  async asyncData ({ app }) {
+    try {
+      const response = await app.$axios.get('/api/comments/')
+      return {
+        comments: response.data.results,
+        error: false
+      }
+    } catch (e) {
+      console.log('error', e)
+      return {
+        comments: [],
+        error: true
+      }
+    }
+  }
+}
+
+</script>
+
+<style>
+
+</style>
